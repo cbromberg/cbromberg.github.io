78ce3c9d1370072907cb1dbc736b6ba3d5e0a52a
Christoffer Bromberg <christoffer@k15t.com>
2014-06-12 20:58:32 +0200

    Blawq

--- prezz/js/prezz.js
+++ prezz/js/prezz.js
@@ -1,4 +1,5 @@
 ALL.getHostJs(function(AP) {
+    window.spaceKey = URI.getQueryParam("spaceKey");
 AP.request({
     url: "/rest/api/space/" + spaceKey + ".json?expand=rootpages",
     success: function (response)
